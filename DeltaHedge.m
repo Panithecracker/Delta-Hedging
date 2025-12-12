05fdde9c7484ee189147dfb7250f6e8bb12b4cd0
Alejandro <97905110+Panithecracker@users.noreply.github.com>
2025-12-12 14:33:54 +0100

    Refactor DeltaHedge.m for improved simulation
    Updated parameters for option pricing and delta hedging simulation. Improved portfolio tracking and added replication error analysis.

--- DeltaHedge.m
+++ DeltaHedge.m
@@ -3,16 +3,16 @@ clc
 format long
 % option parameters:
 T = 1;
-K = 10;
-r = 0.12;
+K = 100;
+r = 0.05;
 type = 0;
 %stock parameters (GBM):
-S0 = 13;
-mu = 1;
+S0 = 100;
+mu = 0;
 sigma = 0.5;
 %DELTA HEDGING OVER MANY SAMPLES:
 hedges = 1;
-N = 1008; %hourly trading for a year
+N = 200; %hourly trading for a year
 t = linspace(0,T,N+1);
 S(1) = S0;
 dt = T/N;
@@ -32,6 +32,7 @@ else
 end
 portfolio = zeros(N+1,1); %replicating portfolio history through [0,T]
 portfolio(1) = option(1);
+replication_error = zeros(N+1,1); %replication error history (most important is the last)
 dW = sigma*sqrt(dt)*randn(N,1); %precompute wiener increments
 for i = 1:N
     %rebalance portfolio using its own wealth (self-financing) N times
@@ -47,7 +48,12 @@ for i = 1:N
         option(i+1) = put;
     end
     borrow = delta*S(i)-portfolio(i); %use bank to borrow[deposit] the remaining[extra] cash
-    portfolio(i+1) = delta*S(i+1)-exp(r*dt)*borrow; %portfolio value for next time 
+    %store portfolio information (share holdings and cash balance)
+    stock(i) = delta;
+    cash(i) = (-1)*borrow;
+    %update portfolio value in the next step based on price change and borrow rate
+    portfolio(i+1) = delta*S(i+1)-exp(r*dt)*borrow;
+    replication_error(i+1) = portfolio(i+1) - option(i+1); 
 end
 pnl_approximate_deviation = (sqrt(0.25*pi)*blsvega(S0,K,r,T,sigma)*sigma)/(sqrt(N)); %approx std of error is prop to option vega at start 
 spread = max(1,2*pnl_approximate_deviation); %extra fee to charge on top of the theoretical price 
@@ -71,6 +77,28 @@ if k == 1 %show specific data of first one only
     ylabel("Price");
     title("Option Replication with Black Scholes");
     legend({'Call option' ,'Portfolio'});
+    %portfolio process (stock and cash evolution) and full history of hedge error
+    figure('Color','w','Position',[200 200 1300 600]);
+    tlo = tiledlayout(2,2);
+    nexttile
+    plot(t(1:N),stock,'b');
+    xlabel("Time");
+    ylabel("Stock holdings")
+    title("Stock Holdings")
+    grid on
+    nexttile
+    plot(t(1:N),cash,'black');
+    ylabel("Cash balance")
+    xlabel("Time")
+    title("Cash Balance")
+    grid on
+    nexttile([1 2])
+    plot(t,replication_error,'r');
+    xlabel('Time')
+    ylabel('Error')
+    title('Replication Error')
+    grid on
+
 %     fprintf("Hedging error : %2f\n",PnL_writer(1)-spread); %discrete hedging error (difference of observed option payoff and portfolio value at T)
 end
 % %remarkably, as we increase the rebalancing frequency (higher N) then the
@@ -136,25 +164,72 @@ end
 %data we can evaluate how good our strategy was by comparing the payoff of the option to our portfolio of stock and bonds and then our charged spread. We sold at this price 
 
 
+%% ALTERNATIVE : BINOMIAL TREES COMPARISON
+[pbin,dbin] = binprice(S0,K,r,T,sigma,N);
+fprintf("Bin price : %.2f$, Initial Delta: %.2f shares\n",pbin+spread,dbin);
 
-%% ALTERNATIVE : BINOMIAL TREES 
-u = exp(sigma*sqrt(dt));
+%compare the black scholes surface and the binomial one for different
+%(price,time) pairs.
+[U,V] = meshgrid(linspace(0,20,101),linspace(0,1,101));
+for i=1:size(U,1)
+    for j=1:size(V,1)
+        BS(i,j) = blsprice(U(i,j),K,r,T-V(i,j),sigma);
+        [a,b] = binprice(U(i,j),K,r,T-V(i,j),sigma,N);
+        Bin(i,j) = a;
+        Bin_del(i,j) = b;
+    end
+end
+%compare price surfaces:
+figure
+surf(U,V,BS);
+xlabel("share price");
+ylabel("time");
+zlabel("option price");
+title("Black-Scholes surface");
+figure
+surf(U,V,Bin);
+xlabel("share price");
+ylabel("time");
+zlabel("option price");
+title("Binomial surface");
+
+
+function [price,delta] = binprice(S0,K,r,T,sigma,N)
+%option 1 (backwards recursion towards time 0)
+%complexity: 0(N^2)
+type = 0;
+dt = T/N;
+u = exp(sigma*sqrt(dt)); %up/down factors
 d = exp((-1)*sigma*sqrt(dt));
-p = (exp(r*dt)-d)/(u-d); 
-for k=N:-1:0 %option values at maturity
+p = (exp(r*dt)-d)/(u-d); %risk neutral probs
+for k=N:-1:0 %maturity values
     if type == 0
         V(k+1) = max(u^k*d^(N-k)*S0-K,0);
     else
         V(k+1) = max(K-u^k*d^(N-k)*S0,0);
     end
 end
-for i=N:-1:1
+for i=N:-1:1 %backward recursion
     if i == 1 %final iteration, compute delta before losing it:
-        D = (V(2)-V(1))/((u-d)*S0);
+        delta = (V(2)-V(1))/((u-d)*S0);
     end
     for j=1:i
         V(j) = exp(-r*dt)*(p*V(j+1)+(1-p)*V(j));
     end
 end
-fprintf("Bin price : %.2f$, Initial Delta: %.2f shares\n",V(1)+spread,D);
+%return the price and delta
+price = V(1);
+
+%option 2: summation formula
+%complexity: 0(N)
+% price = 0;
+% for k = 0:N
+%     payoff = max(S0*(u^k)*(d^(N-k)) - K, 0);
+%     price  = price + nchoosek(N,k) * p^k * (1-p)^(N-k) * payoff;
+% end
+% 
+% price = exp(-r*T) * price;
+% delta = 0;
+end
+
 
